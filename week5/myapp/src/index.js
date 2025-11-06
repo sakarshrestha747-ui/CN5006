35a92510ec54943fe8c371faf94d79f02a94b9a9
sakarshrestha747-ui <sakarshrestha747@gmail.com>
2025-11-06 09:20:08 +0000

    Commit

--- week5/myapp/src/index.js
+++ week5/myapp/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
-import AppColor from './AppbackgroundColor'
-import reportWebVitals from './reportWebVitals';
+import Calculator from './SimpleCalculator'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppColor heading="This is first element" lbl="Name :" color="green"/>
-    <AppColor heading="This is second element" lbl="Name :" color="blue"/>
-    <AppColor heading="This is third third element" lbl="Name :" color="Yellow"/>
+     <Calculator/>
  </React.StrictMode>
 );
 

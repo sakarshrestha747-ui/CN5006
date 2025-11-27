9a403f4d41e09bcf7722f573ba305e284be15346
sakarshrestha747-ui <sakarshrestha747@gmail.com>
2025-11-27 17:05:50 +0000

    Commit

--- week9/myapp/src/index.js
+++ week9/myapp/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FacebookEmojiCounter from './ Facebookemoji';
+import ToggleMode from './ToggleModeComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <FacebookEmojiCounter type= "Like"/>
+  <FacebookEmojiCounter type= "Love"/>
+  <FacebookEmojiCounter type= "happy"/>
+  <ToggleMode/>
   </React.StrictMode>
 );
 
